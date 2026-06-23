16b162cd3ca6f3f8b7daf304084d960483817156
Tunga Durga Sai Prasad <152528866+durgasaiprasad06@users.noreply.github.com>
2026-06-23 23:21:57 +0530

    Update ProductOfArrayExceptSelf.java

--- ProductOfArrayExceptSelf.java
+++ ProductOfArrayExceptSelf.java
@@ -1,5 +1,7 @@
-#java
-
+/**
+ * Problem Name : 238. Product of Array Except Self
+ * Link : https://leetcode.com/problems/product-of-array-except-self/
+ */
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
